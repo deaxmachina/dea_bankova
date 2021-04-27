27296a96b518a72d8d645b98e15fc92e34348771
deaxmachina <d.y.bankova@gmail.com>
2021-04-27 09:45:21 +0900

    reverting back to old hero page design

--- src/Components/HeroPage/HeroText.js
+++ src/Components/HeroPage/HeroText.js
@@ -3,13 +3,12 @@ import React from "react";
 const HeroText = () => {
   return (
     <div id="hero-text-container" >
-      {/*<p className="greeting highlight-dark-background">Hi, I'm</p>*/}
+      <p className="greeting highlight-dark-background">Hi, I'm</p>
       <h1 className="name highlight-dark-background">Dea Bankova</h1>
       <p className="personal-description highlight-dark-background">
-        Data Visualisation Designer
-        <br />
-         <span>Developer | ex Data Scientist</span>
-        <br ></br>
+      I create visualisations to tell stories with data.  
+        Currently Project Researcher in data visualisation 
+        at the University of Tokyo & independent
 
       </p>
     </div>
