1072e942ea5c809c65004509fdce36053d0e3b8c
deaxmachina <d.y.bankova@gmail.com>
2021-04-27 09:34:52 +0900

    testing banner 3

--- src/Components/HeroPage/HeroText.js
+++ src/Components/HeroPage/HeroText.js
@@ -6,8 +6,9 @@ const HeroText = () => {
       {/*<p className="greeting highlight-dark-background">Hi, I'm</p>*/}
       <h1 className="name highlight-dark-background">Dea Bankova</h1>
       <p className="personal-description highlight-dark-background">
-        Data Visualisation 
-        Designer | Developer 
+        Data Visualisation Designer
+        <br />
+         <span>Developer | ex Data Scientist</span>
         <br ></br>
 
       </p>
