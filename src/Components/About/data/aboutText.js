2351335946efa3c8639b2dd42a5807ca55d6968e
deaxmachina <d.y.bankova@gmail.com>
2021-05-02 12:59:08 +0900

    modified about section dimensions

--- src/Components/About/data/aboutText.js
+++ src/Components/About/data/aboutText.js
@@ -9,7 +9,7 @@ const aboutString =
   <br />
   <p>
     I have worked in both industry and academia and have a background in mathematics and machine learning. While my 
-    focus is on data visualisation, I can engage in the end-to-end process of working with data - 
+    focus is on data visualisation, I can engage in the end-to-end or part of the process of working with data - 
     posing a question, extracting insight, designing the visual representation, and finally coding it all up. 
     My technical stack includes Python for data analysis, JavaScript and React for front-end, and D3.js for building custom visualisations. 
   </p>
