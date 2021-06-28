a19ed8fd237b5e21fe202636f4ce46b456646b94
deaxmachina <d.y.bankova@gmail.com>
2021-06-28 14:52:31 +0900

    added co2 trees project

--- src/data/visualisationsMetadata.js
+++ src/data/visualisationsMetadata.js
@@ -1,5 +1,31 @@
 const visualisationsMetadata = [
   {
+    id: 'co2-trees', 
+    title: "Trees and CO2 in the atmosphere - data art with p5.js",
+    websiteUrl: "https://deaxmachina.github.io/p5_trees/",
+    imgUrl: 'https://live.staticflickr.com/65535/51276665860_581042e6c9_o.png',
+    smallImgUrl: 'https://live.staticflickr.com/65535/51276665860_581042e6c9_o.png',
+    individualPageImgUrl: "https://live.staticflickr.com/65535/51276665860_581042e6c9_o.png",
+    individualPageSmallImgUrl: 'https://live.staticflickr.com/65535/51276665860_581042e6c9_o.png',
+    backgroundColour: "#219ebc",
+    responsibleFor: 'visualisation, website',
+    tools: 'p5.js, JavaScript',
+    description: ""
+  },
+  {
+    id: 'words-lifetime', 
+    title: "Words we say in a lifetime - scrollytelling with D3.js",
+    websiteUrl: "https://lifetimeofwords.netlify.app/",
+    imgUrl: 'https://live.staticflickr.com/65535/51182741664_87330e57a0_o.jpg',
+    smallImgUrl: 'https://live.staticflickr.com/65535/51182741664_87330e57a0_o.jpg',
+    individualPageImgUrl: "https://live.staticflickr.com/65535/51183053540_9ed8338364_o.png",
+    individualPageSmallImgUrl: 'https://live.staticflickr.com/65535/51183053540_9ed8338364_o.png',
+    backgroundColour: "#805880",
+    responsibleFor: 'data preparation, visualisation, website',
+    tools: 'D3.js, GSAP, Python',
+    description: ""
+  },
+  {
     id: 'sakura-kyoto', 
     title: "Cherry Blossom Bloom Times in Kyoto",
     websiteUrl: "https://deaxmachina.github.io/sakura_bloom_times/",
@@ -10,19 +36,6 @@ const visualisationsMetadata = [
     backgroundColour: "#e27396",
     responsibleFor: 'data preparation, visualisation, website',
     tools: 'D3.js, Python',
-    description: ""
-  },
-  {
-    id: 'words-lifetime', 
-    title: "Words we say in a lifetime - scrollytelling",
-    websiteUrl: "https://lifetimeofwords.netlify.app/",
-    imgUrl: 'https://live.staticflickr.com/65535/51182741664_87330e57a0_o.jpg',
-    smallImgUrl: 'https://live.staticflickr.com/65535/51182741664_87330e57a0_o.jpg',
-    individualPageImgUrl: "https://live.staticflickr.com/65535/51183053540_9ed8338364_o.png",
-    individualPageSmallImgUrl: 'https://live.staticflickr.com/65535/51183053540_9ed8338364_o.png',
-    backgroundColour: "#805880",
-    responsibleFor: 'data preparation, visualisation, website',
-    tools: 'D3.js, GSAP, Python',
     description: ""
   }
 ]
