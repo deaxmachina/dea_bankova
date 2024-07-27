83b9a315b073c0061a1fc5c764c44637df84e914
Dea Bankova <d.y.bankova@gmail.com>
2024-07-27 17:11:23 +0100

    added filter controls

--- src/Components/PortfolioFull/AllProjectsWithText.js
+++ src/Components/PortfolioFull/AllProjectsWithText.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import * as _ from 'lodash';
 import { Link } from "react-router-dom";
 import PortpolioPopup from "../PortfolioPage/PortfolioPopup";
 import Video from './Video'
+import FilterAndExplainText from "./FilterAndExplainText";
 
 // at what point should the image swich to the smaller one? 
 const minToChangeImage = 450; 
 
 const AllProjects = ({ projectsMetadata, sectionTitle, sectionSubtitle, windowWidth, selectedProject, setSelectedProject }) => {
+
+  const [filterTech, setFilterTech] = useState('all')
+  const [filterProjectType, setFilterProjectType] = useState('all')
+
+  const selectedProjectsMetadata = filterTech === 'all' && filterProjectType === 'all'
+    ? projectsMetadata 
+    : filterTech === 'all' && filterProjectType !== 'all'
+    ? projectsMetadata.filter(d => d.projectTypeArray.includes(filterProjectType))
+    : filterTech !== 'all' && filterProjectType === 'all'
+    ? projectsMetadata.filter(d => d.toolsArray.includes(filterTech))
+    : projectsMetadata
+
+  // const selectedProjectsMetadata = projectsMetadata
 
   return (
     <>
@@ -17,11 +32,20 @@ const AllProjects = ({ projectsMetadata, sectionTitle, sectionSubtitle, windowWi
           </div>
           
         </div>
-        <div class='section-subtitle'>{sectionSubtitle}</div>
+        <div class='section-subtitle'>
+          <FilterAndExplainText 
+            filterTech={filterTech} 
+            setFilterTech={setFilterTech} 
+            filterProjectType={filterProjectType}
+            setFilterProjectType={setFilterProjectType} 
+          />  
+        </div>
         <div className="clarifying-message">click on each image for details</div>
+
+        {/* Large projects */}
         <div className="portfolio-grid">
           {
-            projectsMetadata.map(project => ( 
+            selectedProjectsMetadata.filter(d => d.size === 'full').map(project => ( 
               <div className='portfolio-grid-entry-wrapper'>
                 <div 
                   key={project.id} 
@@ -69,6 +93,40 @@ const AllProjects = ({ projectsMetadata, sectionTitle, sectionSubtitle, windowWi
             ))
           }
         </div>
+
+        {/* Smaller projects */}
+        <div className="portfolio-grid smaller-projects">
+          {
+            selectedProjectsMetadata.filter(d => d.size !== 'full').map(project => ( 
+              <div className='portfolio-grid-entry-wrapper'>
+                <div 
+                  key={project.id} 
+                  className={`portfolio-grid-entry ${project.size}`}
+                  style={{backgroundImage: windowWidth > minToChangeImage? `url(${project.smallImgUrl})` : `url(${project.imgUrl})`}}
+                  >
+                  {project.videoUrl ? <Video videoUrl={project.videoUrl} /> : ''}
+                  <button className="portfolio-grid-btn full-project noSelect" >
+                    <span className="portfolio-grid-project-title body-text-large">{project.title}</span>
+                    <br/>
+                    {
+                      project.hasPage ? 
+                      <span 
+                        className="border-button full-project-btn noSelect" 
+                        onClick={() => setSelectedProject(project)}>
+                          about
+                      </span> 
+                      : 
+                      <a href={project.websiteUrl} target="_blank" className="noSelect">
+                        <span className="border-button full-project-btn noSelect">view</span>
+                      </a>
+                    }             
+                  </button>
+                </div>    
+              </div>
+            ))
+          }
+        </div>
+
       </div>
       {
         selectedProject && 
---
+++ src/Components/PortfolioFull/FilterAndExplainText.js
@@ -0,0 +1,124 @@
+
+const FilterAndExplainText = ({ filterTech, setFilterTech, filterProjectType, setFilterProjectType }) => {
+
+  return (
+    <div className="wrapper-filter-and-explain-text">
+    My versatile portfolio includes works in: 
+    <span 
+      className={`filter-link ${filterProjectType === 'data visualisation' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterProjectType('data visualisation');
+        setFilterTech('all') 
+      }}>
+      data visualisation
+    </span>, 
+    <span 
+      className={`filter-link ${filterProjectType === 'data art' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterProjectType('data art')
+        setFilterTech('all')
+      }}>
+      data art
+    </span>, 
+    <span 
+      className={`filter-link ${filterProjectType === 'creative coding' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterProjectType('creative coding')
+        setFilterTech('all')
+      }}
+    >
+      creative coding
+    </span>, 
+    <span 
+      className={`filter-link ${filterProjectType === 'front-end' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterProjectType('front-end') 
+        setFilterTech('all')
+      }}>
+      front-end
+    </span> 
+    <button onClick={() => { setFilterProjectType('all') }}>(reset)</button>
+
+
+    <br></br>
+    I often use JavaScript frameworks like 
+    <span 
+      className={`filter-link ${filterTech === 'Svelte' && 'active'}`}
+      tabIndex={0} 
+      role='button'
+      onClick={() => { 
+        setFilterTech('Svelte') 
+        setFilterProjectType('all')
+      }}
+    >
+      Svelte
+    </span> and 
+    <span 
+      className={`filter-link ${filterTech === 'React' && 'active'}`} 
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterTech('React') 
+        setFilterProjectType('all')
+      }}
+    >
+      React
+    </span>, 
+    and libraries such as 
+    <span 
+      className={`filter-link ${filterTech === 'D3.js' && 'active'}`} 
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterTech('D3.js') 
+        setFilterProjectType('all')
+      }}>
+      D3.js
+    </span>, 
+    <span 
+      className={`filter-link ${filterTech === 'HTML5 Canvas' && 'active'}`}  
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterTech('HTML5 Canvas') 
+        setFilterProjectType('all')
+    }}>
+      Canvas API
+    </span>, 
+    <span 
+      className={`filter-link ${filterTech === 'p5.js' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterTech('p5.js') 
+        setFilterProjectType('all')
+    }}>
+      p5.js
+    </span>, 
+    <span 
+      className={`filter-link ${filterTech === 'Three.js' && 'active'}`}
+      tabIndex={0} 
+      role='button' 
+      onClick={() => { 
+        setFilterTech('Three.js') 
+        setFilterProjectType('all')
+      }}>
+      Three.js
+    </span>
+    <button onClick={() => { setFilterTech('all') }}>(reset)</button>
+
+    <br></br>
+    Large image = project I did on my own or had a crucial role. Smaller image = project smaller in scope or my involvement. 
+    </div>
+  )
+}
+
+export default FilterAndExplainText
