c949f3e7dfcb9f07102820f8b487f3231ac49be7
deaxmachina <Dea.Bankova@thomsonreuters.com>
2024-12-26 10:40:13 +0200

    more in development

--- src/Components/PortfolioPage/USElections/Development.js
+++ src/Components/PortfolioPage/USElections/Development.js
@@ -2,9 +2,64 @@ import "./USElections.css";
 import { useState, useEffect } from 'react'
 import { annotate, annotationGroup } from 'rough-notation';
 
+const patternImages = [
+    {
+        id: 'one',
+        srcSmall: 'images/us-elections/custom layers/patterns-1.png',
+        src: 'images/us-elections/custom layers/patterns-1.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+    {
+        id: 'two',
+        srcSmall: 'images/us-elections/custom layers/patterns-2.png',
+        src: 'images/us-elections/custom layers/patterns-2.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+    {
+        id: 'three',
+        srcSmall: 'images/us-elections/custom layers/patterns-3.png',
+        src: 'images/us-elections/custom layers/patterns-3.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+    {
+        id: 'four',
+        srcSmall: 'images/us-elections/custom layers/patterns-4.png',
+        src: 'images/us-elections/custom layers/patterns-4.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+    {
+        id: 'five',
+        srcSmall: 'images/us-elections/custom layers/patterns-5.png',
+        src: 'images/us-elections/custom layers/patterns-5.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+    {
+        id: 'six',
+        srcSmall: 'images/us-elections/custom layers/patterns-6.png',
+        src: 'images/us-elections/custom layers/patterns-6.png',
+        alt: '',
+        rotate: Math.random()*20 - 10
+    },
+]
 
 const Development = ({ children }) => {
+    const [transformOrigin, setTransformOrigin] = useState("center");
 
+    const handleMouseMove = (e) => {
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = ((e.clientX - rect.left) / rect.width) * 100;
+      const y = ((e.clientY - rect.top) / rect.height) * 100;
+      setTransformOrigin(`${x}% ${y}%`);
+    };
+  
+    const handleMouseLeave = () => {
+      setTransformOrigin("center");
+    };
 
     return (
         <div class='text-well'>
@@ -22,34 +77,55 @@ const Development = ({ children }) => {
             </p>
 
             {/* Custom layers */}
-            <div><span className='listing highlight'>Custom layers</span> supporting various pattern fills, as well as circle layers.</div>
+            <div className="text">
+                <span className='listing highlight'>Custom layers</span> supporting various pattern fills, as well as circle layers.
+            </div>
+            <div className="images-grid">
+            {
+                patternImages.map((img, idx) => (
+                    <div 
+                        className="grid-item" 
+                        style={{  }}
+                        onClick={() => {
+                        }}
+                        onMouseMove={handleMouseMove}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <img src={img.srcSmall ? img.srcSmall : img.src} alt={img.alt}  style={{ transformOrigin }} />
+                    </div>
+                ))
+            }
+            </div>
             
             {/* Custom interaction behaviours */}
-            <div><span className='listing highlight'>Custom interaction behaviours</span> for an improved user experience, including: </div>
-            <ul>
-                <li>Greying out neighbouring states when the user zooms into a given state (screenshot of greyed out state)</li>
-                <li>Always centring first on a state when it is clicked, then centring on a county when that is subsequently clicked (video of zoom behaviours)</li>
-                <li>Allowing further click-to-zoom on a county for more granular interactions with smaller counties and townships (video of double click to zoom)</li> 
-            </ul>
+            <div className="text">
+                <span className='listing highlight'>Custom interaction behaviours</span> for an improved user experience, including
+                <ul>
+                    <li>Greying out neighbouring states when the user zooms into a given state (screenshot of greyed out state)</li>
+                    <li>Always centring first on a state when it is clicked, then centring on a county when that is subsequently clicked (video of zoom behaviours)</li>
+                    <li>Allowing further click-to-zoom on a county for more granular interactions with smaller counties and townships (video of double click to zoom)</li> 
+                </ul>
+            </div>
 
             {/* Legends */}
             <div className="text">
-                <span className='listing highlight'>Separate legends for each map</span>
-                , dynamically changing depending on whether the user has clicked on a state or is viewing 
+                <span className='listing highlight'>Separate legends for each map</span>, dynamically changing depending on whether the user has clicked on a state or is viewing 
                 the map in its entirety. This ensures only the relevant legend is shown at each stage, avoiding clutter on the page.
             </div>
 
             {/* Tooltips */}
-            <div className="text"><span className='listing highlight'>Custom tooltips</span> transforming into modals on mobile</div>
-            <ul>
-                <li>I implemented approximately half of the logic for the content of all tooltips and all of the logic determining how they should behave differently on desktop and mobile.</li>
-                <li>Highly customisable tooltips / modals that behave differently on our Reuters page and client embeds</li>
-                <li>For mobile, I developed two options (for dotcom and embeds): one where the modal sticks to the bottom of 
-                    the page when the user navigates to a geographic feature, and another where the modal is persistently placed 
-                    just below the map in the same scenario. These options required distinct technical considerations, 
-                    especially around persisting the highlighted state/county/district to ensure a seamless user experience.
-                </li>
-            </ul>
+            <div className="text">
+                <span className='listing highlight'>Custom tooltips</span> transforming into modals on mobile
+                <ul>
+                    <li>I implemented approximately half of the logic for the content of all tooltips and all of the logic determining how they should behave differently on desktop and mobile.</li>
+                    <li>Highly customisable tooltips / modals that behave differently on our Reuters page and client embeds</li>
+                    <li>For mobile, I developed two options (for dotcom and embeds): one where the modal sticks to the bottom of 
+                        the page when the user navigates to a geographic feature, and another where the modal is persistently placed 
+                        just below the map in the same scenario. These options required distinct technical considerations, 
+                        especially around persisting the highlighted state/county/district to ensure a seamless user experience.
+                    </li>
+                </ul>
+            </div>
             <div className="container-tooltips-videos">
                 <video 
                     width="250" 
