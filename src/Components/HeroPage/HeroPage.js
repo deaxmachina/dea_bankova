a354530e1a55b7efd8f43c42cfcff0c28c527219
Dea Bankova <d.y.bankova@gmail.com>
2025-08-07 20:23:04 +0100

    try to fix videos

--- src/Components/HeroPage/HeroPage.js
+++ src/Components/HeroPage/HeroPage.js
@@ -4,19 +4,18 @@ import HeaderViz from "./HeaderViz";
 import HeroText from "./HeroText";
 import HeroMenu from "./HeroMenu";
 
-
 const HeroPage = ({ windowWidth, windowHeight }) => {
   return (
     <section id="hero-section">
       <HeroMenu />
       <HeroText />
-      <HeaderViz 
+      {/* <HeaderViz 
           scaleFactor={ 
             windowWidth > 1500 ? 1: windowWidth > 1000 ? 0.9 : windowWidth > 650 ? 0.75 : windowWidth > 400 ? 0.50 : 0.45
           }
-        />
+        /> */}
     </section>
-  )
+  );
 };
 
-export default HeroPage; 
+export default HeroPage;
